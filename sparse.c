70defd9941362505551d3f14528babaffcf026fb
Alexsabu <alexsabu053@gmail.com>
2025-07-29 16:19:02 +0530

    Update sparse.c

--- sparse.c
+++ sparse.c
@@ -1,90 +1,110 @@
 #include <stdio.h>
 
-#define MAX 100
-
-void inputMatrix(int mat[MAX][MAX], int rows, int cols) {
-    printf("Enter the elements of the matrix (row by row):\n");
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            scanf("%d", &mat[i][j]);
-        }
+int main()
+{
+    int row1, colm1, row2, colm2,mat1[10][10], mat2[10][10],spar1[10][3], spar2[10][3], sparResult[10][3],i, j, k;
+    printf("Enter rows and columns of first matrix: ");
+    scanf("%d %d", &row1, &colm1);
+    printf("Enter rows and columns of second matrix: ");
+    scanf("%d %d", &row2, &colm2);
+    if (row1 != row2 || colm1 != colm2)
+    {
+        printf("Matrices must have the same dimensions for addition.\n");
+        return 0;
     }
-}
-
-void addMatrices(int mat1[MAX][MAX], int mat2[MAX][MAX], int res[MAX][MAX], int rows, int cols) {
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            res[i][j] = mat1[i][j] + mat2[i][j];
-        }
-    }
-}
-
-void transposeMatrix(int mat[MAX][MAX], int transposed[MAX][MAX], int rows, int cols) {
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            transposed[j][i] = mat[i][j];
-        }
-    }
-}
-
-void printMatrix(int mat[MAX][MAX], int rows, int cols) {
-    printf("Matrix form:\n");
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            printf("%d ", mat[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-void printTupleForm(int mat[MAX][MAX], int rows, int cols) {
-    int count = 0;
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            if (mat[i][j] != 0) {
-                count++;
+    printf("Enter elements of first matrix:\n");
+    for(i = 0; i < row1; i++)
+        for(j = 0; j < colm1; j++)
+            scanf("%d", &mat1[i][j]);
+    printf("Enter elements of second matrix:\n");
+    for(i = 0; i < row2; i++)
+        for(j = 0; j < colm2; j++)
+            scanf("%d", &mat2[i][j]);
+    k = 0;
+    for(i = 0; i < row1; i++)
+    {
+        for(j = 0; j < colm1; j++)
+        {
+            if(mat1[i][j] != 0)
+            {
+                spar1[k][0] = i;
+                spar1[k][1] = j;
+                spar1[k][2] = mat1[i][j];
+                k++;
             }
         }
     }
-
-    printf("Tuple form (rows, cols, non-zero values):\n");
-    printf("(%d, %d, %d)\n", rows, cols, count);
-
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            if (mat[i][j] != 0) {
-                printf("(%d, %d, %d)\n", i, j, mat[i][j]);
+    int nonZero1 = k;
+    k = 0;
+    for(i = 0; i < row2; i++)
+    {
+        for(j = 0; j < colm2; j++)
+        {
+            if(mat2[i][j] != 0)
+            {
+                spar2[k][0] = i;
+                spar2[k][1] = j;
+                spar2[k][2] = mat2[i][j];
+                k++;
             }
         }
     }
-}
+    int nonZero2 = k;
+    int p1 = 0, p2 = 0, p3 = 0;
+    while(p1 < nonZero1 && p2 < nonZero2)
+    {
+        int r1 = spar1[p1][0], c1 = spar1[p1][1];
+        int r2 = spar2[p2][0], c2 = spar2[p2][1];
+        if(r1 < r2 || (r1 == r2 && c1 < c2))
+        {
+            sparResult[p3][0] = r1;
+            sparResult[p3][1] = c1;
+            sparResult[p3][2] = spar1[p1][2];
+            p1++;
+            p3++;
+        }
+        else if(r2 < r1 || (r1 == r2 && c2 < c1))
+        {
 
-int main() {
-    int r, c;
-    int mat1[MAX][MAX], mat2[MAX][MAX], result[MAX][MAX], transposed[MAX][MAX];
-
-    printf("Enter the number of rows and columns: ");
-    scanf("%d %d", &r, &c);
-
-    printf("Enter the first matrix:\n");
-    inputMatrix(mat1, r, c);
-    printMatrix(mat1, r, c);
-    printTupleForm(mat1, r, c);
-
-    printf("Enter the second matrix:\n");
-    inputMatrix(mat2, r, c);
-    printMatrix(mat2, r, c);
-    printTupleForm(mat2, r, c);
-
-    addMatrices(mat1, mat2, result, r, c);
-    printf("Resultant matrix (sum of the two matrices):\n");
-    printMatrix(result, r, c);
-    printTupleForm(result, r, c);
-
-    transposeMatrix(result, transposed, r, c);
-    printf("Transpose of the resultant matrix:\n");
-    printMatrix(transposed, c, r);
-    printTupleForm(transposed, c, r);
+            sparResult[p3][0] = r2;
+            sparResult[p3][1] = c2;
+            sparResult[p3][2] = spar2[p2][2];
+            p2++;
+            p3++;
+        }
+        else
+        {
+            int sum = spar1[p1][2] + spar2[p2][2];
+            if(sum != 0) 
+            {
+                sparResult[p3][0] = r1;
+                sparResult[p3][1] = c1;
+                sparResult[p3][2] = sum;
+                p3++;
+            }
+            p1++;
+            p2++;
+        }
+    }
+    while(p1 < nonZero1)
+    {
+        sparResult[p3][0] = spar1[p1][0];
+        sparResult[p3][1] = spar1[p1][1];
+        sparResult[p3][2] = spar1[p1][2];
+        p1++; p3++;
+    }
+    while(p2 < nonZero2)
+    {
+        sparResult[p3][0] = spar2[p2][0];
+        sparResult[p3][1] = spar2[p2][1];
+        sparResult[p3][2] = spar2[p2][2];
+        p2++; p3++;
+    }
+    printf("\nResultant Sparse Matrix:\n");
+    for(i = 0; i < p3; i++)
+    {
+        printf("%d %d %d\n", sparResult[i][0], sparResult[i][1], sparResult[i][2]);
+    }
 
     return 0;
 }
